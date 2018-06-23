38846911d02824d27bd4ca3dfa28a7c3e5ea3aa3
Your Name <proman3419@gmail.com>
2018-06-23 22:51:26 +0200

    Persons classes no longer contain the database of words

--- 10 Random sentence generator/10 Random sentence generator/Program.cs
+++ 10 Random sentence generator/10 Random sentence generator/Program.cs
@@ -10,116 +10,100 @@ namespace _10_Random_sentence_generator
     {
         string[] Name { get; set; }
         string[] Be { get; set; }
-        string[] PastSimple { get; set; }
-        string[] PresentSimple { get; set; }
-        string[] Continous { get; set; }
-        string[] Words { get; set; }
+        int NameHighestId { get; set; }
     }
 
-    class Initializer
+    class WordBase
     {
-        public static int PastSimpleHighestId { get; set; }
-        public static int PresentSimpleHighestId { get; set; }
-        public static int ContinousHighestId { get; set; }
-        public static int WordsHighestId{ get; set; }
-
-        static public string[] InitializePastSimple()
-        {
-            string[] temp = new string[] { "killed", "ate", "edited", "walked by", "walked down", "missed", "added", "created", "drinked", "burnt", "fried", "looted", "drived",
-            "climbed", "destroyed", "described", "fought with", "passed"};
-            PastSimpleHighestId = temp.GetUpperBound(0);
-            return temp;
-        }
-
-        static public string[] InitializePresentSimple()
-        {
-            string[] temp = new string[] { "kill", "eat", "edit", "walk by", "walk down", "miss", "add", "create", "drink", "burn", "fry", "loot", "drive", "climb", "destroy", "describe", "fight with", "pass" };
-            PresentSimpleHighestId = temp.GetUpperBound(0);
-            return temp;
-        }
-
-        static public string[] InitializeContinous()
-        {
-            string[] temp = new string[] { "killing", "eating", "editing", "walking by", "walking down", "missing", "adding", "creating", "drinking", "burning", "looting", "driving", "climbing",
-            "destroying", "describing", "fighting with", "passing" };
-            ContinousHighestId = temp.GetUpperBound(0);
-            return temp;
-        }
-
-        static public string[] InitializeWords()
-        {
-            string[] temp = new string[] {"a homeless cat", "a damaged car", "a kangaroo from Australia", "an important text", "a dinner", "an apple", "a house with a green roof",
-            "water", "the nearby woods", "the longest river in the country", "fries", "a machine oil", "the bag of a passerby", "a machine gun", "a massive rock", "an angry bear",
-            "a bloodthirsty wolf", "a sleepy panda", "an annoying kid", "a fancy dress", "a delicious dish", "a dried out river", "a deep canyon", "a steep hill"};
-            WordsHighestId = temp.GetUpperBound(0);
-            return temp;
-        }
-    }
-
-    #region Persons
-    class I : Initializer, IPerson
-    {
-        public string[] Name { get; set; }
-        public int NameHighestId { get; set; }
-        public string[] Be { get; set; }
         public string[] PastSimple { get; set; }
         public string[] PresentSimple { get; set; }
         public string[] Continous { get; set; }
         public string[] Words { get; set; }
+
+        public int PastSimpleHighestId { get; set; }
+        public int PresentSimpleHighestId { get; set; }
+        public int ContinousHighestId { get; set; }
+        public int WordsHighestId{ get; set; }
+
+        public WordBase()
+        {
+            InitializePastSimple();
+            InitializePresentSimple();
+            InitializeContinous();
+            InitializeWords();
+        }
+
+        #region Initializers
+        public void InitializePastSimple()
+        {
+            PastSimple = new string[] { "killed", "ate", "edited", "walked by", "walked down", "missed", "added", "created", "drinked", "burnt", "fried", "looted", "drived",
+            "climbed", "destroyed", "described", "fought with", "passed"};
+            PastSimpleHighestId = PastSimple.GetUpperBound(0);
+        }
+
+        public void InitializePresentSimple()
+        {
+            PresentSimple = new string[] { "kill", "eat", "edit", "walk by", "walk down", "miss", "add", "create", "drink", "burn", "fry", "loot", "drive", "climb", "destroy", "describe", "fight with", "pass" };
+            PresentSimpleHighestId = PresentSimple.GetUpperBound(0);
+        }
+
+        public void InitializeContinous()
+        {
+            Continous = new string[] { "killing", "eating", "editing", "walking by", "walking down", "missing", "adding", "creating", "drinking", "burning", "looting", "driving", "climbing",
+            "destroying", "describing", "fighting with", "passing" };
+            ContinousHighestId = Continous.GetUpperBound(0);
+        }
+
+        public void InitializeWords()
+        {
+            Words = new string[] {"a homeless cat", "a damaged car", "a kangaroo from Australia", "an important text", "a dinner", "an apple", "a house with a green roof",
+            "water", "the nearby woods", "the longest river in the country", "fries", "a machine oil", "the bag of a passerby", "a machine gun", "a massive rock", "an angry bear",
+            "a bloodthirsty wolf", "a sleepy panda", "an annoying kid", "a fancy dress", "a delicious dish", "a dried out river", "a deep canyon", "a steep hill"};
+            WordsHighestId = Words.GetUpperBound(0);
+        }
+        #endregion
+    }
+
+    #region Persons
+    class I : IPerson
+    {
+        public string[] Name { get; set; }
+        public string[] Be { get; set; }
+        public int NameHighestId { get; set; }        
 
         public I()
         {
             Name = new string[] { "I" };
-            NameHighestId = Name.GetUpperBound(0);
             Be = new string[] { "was", "am" };
-            PastSimple = InitializePastSimple();
-            PresentSimple = InitializePresentSimple();
-            Continous = InitializeContinous();
-            Words = InitializeWords();
+            NameHighestId = Name.GetUpperBound(0);
         }
     }
 
-    class HeSheIt : Initializer, IPerson
+    class HeSheIt : IPerson
     {
         public string[] Name { get; set; }
-        public int NameHighestId { get; set; }
         public string[] Be { get; set; }
-        public string[] PastSimple { get; set; }
-        public string[] PresentSimple { get; set; }
-        public string[] Continous { get; set; }
-        public string[] Words { get; set; }
+        public int NameHighestId { get; set; }
 
         public HeSheIt()
         {
             Name = new string[] { "He", "She", "It" };
-            NameHighestId = Name.GetUpperBound(0);
             Be = new string[] { "was", "is"};
-            PastSimple = InitializePastSimple();
-            PresentSimple = InitializePresentSimple();
-            Continous = InitializeContinous();
-            Words = InitializeWords();
+            NameHighestId = Name.GetUpperBound(0);
         }
     }
 
-    class WeTheyYou : Initializer, IPerson
+    class WeTheyYou : IPerson
     {
         public string[] Name { get; set; }
-        public int NameHighestId { get; set; }
         public string[] Be { get; set; }
-        public string[] PastSimple { get; set; }
-        public string[] PresentSimple { get; set; }
-        public string[] Continous { get; set; }
-        public string[] Words { get; set; }
+        public int NameHighestId { get; set; }
 
         public WeTheyYou()
         {
             Name = new string[] { "We", "They", "You" };
-            NameHighestId = Name.GetUpperBound(0);
             Be = new string[] { "were", "are" };
-            PastSimple = InitializePastSimple();
-            PresentSimple = InitializePresentSimple();
-            Continous = InitializeContinous();
-            Words = InitializeWords();
+            NameHighestId = Name.GetUpperBound(0);
         }
     }
     #endregion
@@ -149,34 +133,35 @@ namespace _10_Random_sentence_generator
         public void CreateSentence(dynamic person)
         {  
             Random random = new Random();
+            WordBase wordBase = new WordBase();
             int tense = random.Next(0, 4);
-            int word = random.Next(0, Initializer.WordsHighestId);
+            int word = random.Next(0, wordBase.WordsHighestId);
             int verb, name;
             string temp;
             switch (tense)
             {
                 case 0:
-                    verb = random.Next(0, Initializer.ContinousHighestId);
+                    verb = random.Next(0, wordBase.PastSimpleHighestId);
                     name = random.Next(0, person.NameHighestId);
-                    temp = person.Name[name] + " " + person.Be[0] + " " + person.Continous[verb] + " " + person.Words[word];
+                    temp = person.Name[name] + " " + wordBase.PastSimple[verb] + " " + wordBase.Words[word];
                     Console.WriteLine(temp);
                     break;
                 case 1:
-                    verb = random.Next(0, Initializer.ContinousHighestId);
+                    verb = random.Next(0, wordBase.PresentSimpleHighestId);
                     name = random.Next(0, person.NameHighestId);
-                    temp = person.Name[name] + " " + person.Be[1] + " " + person.Continous[verb] + " " + person.Words[word];
+                    temp = person.Name[name] + " " + wordBase.PresentSimple[verb] + " " + wordBase.Words[word];
                     Console.WriteLine(temp);
                     break;
                 case 2:
-                    verb = random.Next(0, Initializer.PastSimpleHighestId);
+                    verb = random.Next(0, wordBase.ContinousHighestId);
                     name = random.Next(0, person.NameHighestId);
-                    temp = person.Name[name] + " " + person.PastSimple[verb] + " " + person.Words[word];
+                    temp = person.Name[name] + " " + person.Be[0] + " " + wordBase.Continous[verb] + " " + wordBase.Words[word];
                     Console.WriteLine(temp);
                     break;
                 case 3:
-                    verb = random.Next(0, Initializer.PresentSimpleHighestId);
+                    verb = random.Next(0, wordBase.ContinousHighestId);
                     name = random.Next(0, person.NameHighestId);
-                    temp = person.Name[name] + " " + person.PresentSimple[verb] + " " + person.Words[word];
+                    temp = person.Name[name] + " " + person.Be[1] + " " + wordBase.Continous[verb] + " " + wordBase.Words[word];
                     Console.WriteLine(temp);
                     break;
                 default:
