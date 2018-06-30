b2b4bdc001a338c26a1c76bb8ddfd3d63f4f0267
Your Name <proman3419@gmail.com>
2018-07-01 00:09:38 +0200

    16

---
+++ 16 Reverse a string/16 Reverse a string/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Reverse_a_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string to reverse");
+            char[] input = Console.ReadLine().ToCharArray();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = input.GetUpperBound(0); i >= 0; i--)
+                stringBuilder.Append(input[i]);
+            Console.WriteLine("Reversed string: \n{0}", stringBuilder.ToString());
+            Console.ReadLine();
+        }
+    }
+}
