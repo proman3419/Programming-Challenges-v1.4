90f8bac061088ca6f7c39f4e1e3f7d2715dbb428
Your Name <proman3419@gmail.com>
2018-07-06 01:42:54 +0200

    Added: win/lose, fixed generating boards

--- 20 Minesweeper/20 Minesweeper/MainWindow.xaml.cs
+++ 20 Minesweeper/20 Minesweeper/MainWindow.xaml.cs
@@ -33,50 +33,156 @@ namespace _20_Minesweeper
             HiddenContent = String.Empty;
         }
 
-        public void OnClick(Button clickedField)
+        public bool OnClick(Button clickedField, bool recursiveClick)
         {
-            if (!IsRevealed)
+            if (!IsMine || !recursiveClick)
             {
-                IsRevealed = true;
-                clickedField.Content = HiddenContent;
-                clickedField.Background = Brushes.LightGreen;
+                if (MinesNearby == 0 || !recursiveClick)
+                {
+                    if (!IsRevealed)
+                    {
+                        RevealField(this);
+                        return true;
+                    }
+                }
             }
+            return false;
+        }
+
+        public void FirstMove(Field[] fields, int fieldsAmount, int fieldsInARow)
+        {
+            if (!IsMine)
+            {
+                int i = int.Parse(Tag.ToString());
+                try // Up
+                {
+                    if (!(i < fieldsInARow - 1)) // Without the first row
+                        if (!fields[i - fieldsInARow].IsMine)
+                            RevealField(fields[i - fieldsInARow]);
+                }
+                catch { }
+                try // Left
+                {
+                    if (!(i % fieldsInARow == 0)) // Without the first column
+                        if (!fields[i - 1].IsMine)
+                            RevealField(fields[i - 1]);
+                }
+                catch { }
+                try // Right
+                {
+                    if (!(i % fieldsInARow == fieldsInARow - 1)) // Without the last colum
+                        if (!fields[i + 1].IsMine)
+                            RevealField(fields[i + 1]);
+                }
+                catch { }
+                try // Down
+                {
+                    if (!((fieldsAmount - fieldsInARow) < i && i < (fieldsAmount - 1))) // Without the last row
+                        if (!fields[i + fieldsInARow].IsMine)
+                            RevealField(fields[i + fieldsInARow]);
+                }
+                catch { }
+            }
+        }
+
+        public void RevealField(Field field)
+        {
+            field.IsRevealed = true;
+            field.Content = field.HiddenContent;
+            if (field.IsMine)
+                field.Background = Brushes.Red;
+            else
+                field.Background = Brushes.LightGreen;
         }
     }
 
     class Game
     {
         WrapPanel Board { get; set; }
+        TextBlock GameResult { get; set; }
         Field[] Fields { get; set; }
-        int Mines { get; set; }
+        int[] MinesPositions { get; set; }
+        int MinesAmount { get; set; }
         int Difficulty { get; set; }
         int FieldSize { get; set; }
         int FieldsAmount { get; set; }
         int FieldsInARow { get; set; }
+        bool FirstMove { get; set; }
 
-        public Game(WrapPanel board)
+        public Game(WrapPanel board, TextBlock gameResult)
         {
             Difficulty = 0;
             Board = board;
+            GameResult = gameResult;
+            GameResult.VerticalAlignment = VerticalAlignment.Center;
+            GameResult.HorizontalAlignment = HorizontalAlignment.Center;
+            GameResult.Padding = new Thickness(10, 10, 10, 10);
+            FirstMove = true;
             CalculateVariables();
             CreateBoard();
         }
 
+        #region Compositional functions
+        public void ChangeDifficulty(Button difficultyButton)
+        {
+            if (Difficulty == 2)
+            {
+                Difficulty = 0;
+                difficultyButton.Content = "Easy";
+                return;
+            }
+
+            Difficulty++;
+            if (Difficulty == 1)
+                difficultyButton.Content = "Medium";
+            if (Difficulty == 2)
+                difficultyButton.Content = "Hard";
+        }
+
+        public void ResetGame()
+        {
+            FirstMove = true;
+            GameResult.Text = "";
+            GameResult.Background = Brushes.White;
+            CalculateVariables();
+            Board.Children.Clear();
+            CreateBoard();
+        }
+
+        void GameOver()
+        {
+            foreach (var field in Fields)
+                field.RevealField(field);
+            GameResult.Text = "You lose";
+            GameResult.Background = Brushes.Red;
+        }
+
+        void Victory()
+        {
+            foreach (var mine in MinesPositions)
+                if (!Fields[mine].IsMarked)
+                    return;
+            GameResult.Text = "You win";
+            GameResult.Background = Brushes.LightGreen;
+        }
+        #endregion
+
+        #region Create board
         void CalculateVariables()
         {
             switch (Difficulty)
             {
                 case 0:
                     FieldsAmount = 36;
-                    Mines = 10;
+                    MinesAmount = 10;
                     break;
                 case 1:
-                    FieldsAmount = 81;
-                    Mines = 20;
+                    FieldsAmount = 100;
+                    MinesAmount = 20;
                     break;
                 case 2:
                     FieldsAmount = 144;
-                    Mines = 30;
+                    MinesAmount = 30;
                     break;
             }
             FieldsInARow = (int)Math.Sqrt(FieldsAmount);
@@ -86,7 +192,7 @@ namespace _20_Minesweeper
         public void CreateBoard()
         {
             Fields = new Field[FieldsAmount];
-            int[] minesFieldsIds = PlaceMines();
+            PlaceMines();
 
             for (int i = 0; i < FieldsAmount; i++)
             {
@@ -94,11 +200,14 @@ namespace _20_Minesweeper
                 Fields[i].Height = FieldSize;
                 Fields[i].Width = FieldSize;
                 Fields[i].Tag = i;
+                Fields[i].VerticalAlignment = VerticalAlignment.Center;
+                Fields[i].HorizontalAlignment = HorizontalAlignment.Center;
+                Fields[i].FontSize = 18;
                 Fields[i].Click += new RoutedEventHandler(Field_Click);
-                foreach (var id in minesFieldsIds)
+                foreach (var id in MinesPositions)
                     if (i == id)
                     {
-                        Fields[i].Content = "M";
+                        Fields[i].HiddenContent = "M";
                         Fields[i].IsMine = true;
                     }
                 Board.Children.Add(Fields[i]);
@@ -107,18 +216,12 @@ namespace _20_Minesweeper
             PlaceHints();
         }
 
-        void Field_Click(object sender, RoutedEventArgs e)
-        {
-            Fields[int.Parse(((Button)sender).Tag.ToString())].OnClick((Button)sender);
-        }
-
-        int[] PlaceMines()
+        void PlaceMines()
         {
             Random random = new Random();
-            int[] temp = new int[Mines];
-            for (int i = 0; i < Mines; i++)
-                temp[i] = random.Next(0, FieldsAmount);
-            return temp;
+            MinesPositions = new int[MinesAmount];
+            for (int i = 0; i < MinesAmount; i++)
+                MinesPositions[i] = random.Next(0, FieldsAmount);
         }
 
         void PlaceHints()
@@ -160,29 +263,67 @@ namespace _20_Minesweeper
                 }
             }
         }
+        #endregion
 
-        public void ChangeDifficulty(Button difficultyButton)
+        #region Field click 
+        void Field_Click(object sender, RoutedEventArgs e)
         {
-            if (Difficulty == 2)
+            int clickedFieldId = int.Parse(((Button)sender).Tag.ToString());
+            if (Fields[clickedFieldId].IsMine)
             {
-                Difficulty = 0;
-                difficultyButton.Content = "Easy";
+                Fields[clickedFieldId].RevealField(Fields[clickedFieldId]);
+                GameOver();
                 return;
             }
 
-            Difficulty++;
-            if (Difficulty == 1)
-                difficultyButton.Content = "Medium";
-            if (Difficulty == 2)
-                difficultyButton.Content = "Hard";
+            List<int> revealedFields = new List<int>();
+
+
+            if (Fields[clickedFieldId].OnClick((Button)sender, false))
+                revealedFields.Add(CheckNeighbours(clickedFieldId, revealedFields));
+            if (FirstMove)
+            {
+                foreach (var id in revealedFields)
+                    Fields[id].FirstMove(Fields, FieldsAmount, FieldsInARow);
+                FirstMove = false;
+            }
+
+            Victory();
         }
 
-        public void ResetGame()
+        int CheckNeighbours(int i, List<int> revealedFields)
         {
-            CalculateVariables();
-            Board.Children.Clear();
-            CreateBoard();
+            try // Up
+            {
+                if (!(i < FieldsInARow - 1)) // Without the first row
+                    if (Fields[i - FieldsInARow].OnClick(Fields[i - FieldsInARow], true))
+                        revealedFields.Add(CheckNeighbours(i - FieldsInARow, revealedFields));
+            }
+            catch { }
+            try // Left
+            {
+                if (!(i % FieldsInARow == 0)) // Without the first column
+                    if (Fields[i - 1].OnClick(Fields[i - 1], true))
+                        revealedFields.Add(CheckNeighbours(i - 1, revealedFields));
+            }
+            catch { }
+            try // Right
+            {
+                if (!(i % FieldsInARow == FieldsInARow - 1)) // Without the last colum
+                    if (Fields[i + 1].OnClick(Fields[i + 1], true))
+                        revealedFields.Add(CheckNeighbours(i + 1, revealedFields));
+            }
+            catch { }
+            try // Down
+            {
+                if (!((FieldsAmount - FieldsInARow) < i && i < (FieldsAmount - 1))) // Without the last row
+                    if (Fields[i + FieldsInARow].OnClick(Fields[i + FieldsInARow], true))
+                        revealedFields.Add(CheckNeighbours(i + FieldsInARow, revealedFields));
+            }
+            catch { }
+            return i;
         }
+        #endregion
     }
 
     public partial class MainWindow : Window
@@ -195,12 +336,13 @@ namespace _20_Minesweeper
             Board.Width = 350;
             Board.Height = Board.Width;
 
-            game = new Game(Board);
+            game = new Game(Board, GameResult);
         }
 
+        #region Buttons
         private void NewGame_Click(object sender, RoutedEventArgs e)
         {
-
+            game.ResetGame();
         }
 
         private void Difficulty_Click(object sender, RoutedEventArgs e)
@@ -208,5 +350,6 @@ namespace _20_Minesweeper
             game.ChangeDifficulty((Button)sender);
             game.ResetGame();
         }
+        #endregion
     }
 }
