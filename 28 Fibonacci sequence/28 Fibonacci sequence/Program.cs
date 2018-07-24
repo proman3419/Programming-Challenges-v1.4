ec4b74aa8049c99781169094bad9ac5dd93327e3
Your Name <proman3419@gmail.com>
2018-07-24 19:20:35 +0200

    28

---
+++ 28 Fibonacci sequence/28 Fibonacci sequence/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_Fibonacci_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elements, previousNumber = 0, beforePreviousNumber = 0, currentNumber = 0;
+            Console.WriteLine("How many elemenets of the Fibonacci sequence do you want to get?");
+            while (!int.TryParse(Console.ReadLine(), out elements))
+                Console.WriteLine("Wrong input");
+            for (int i = 0; i < elements; i++)
+            {
+                if (i == 0)
+                    currentNumber = 1;
+                else if (i == 1)
+                    previousNumber = 1;
+                else if (i == 2)
+                    beforePreviousNumber = 1;
+                if(i >= 2)
+                {
+                    currentNumber = previousNumber + beforePreviousNumber;
+                    beforePreviousNumber = previousNumber;
+                    previousNumber = currentNumber;
+                }
+                Console.Write(currentNumber + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
