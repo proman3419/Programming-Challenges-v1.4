3c180ec19f1ec4adb93ea2f2745a4c4d2044fe03
Your Name <proman3419@gmail.com>
2018-07-27 15:25:44 +0200

    Finished

--- 31 Blackjack/31 Blackjack/Program.cs
+++ 31 Blackjack/31 Blackjack/Program.cs
@@ -63,11 +63,18 @@ namespace _31_Blackjack
                 Console.Write(segment);
         }
 
-        public static List<List<Card>> ResetPlayerAndCroupier()
+        public static List<Hand> ResetPlayerAndCroupier()
         {
-            List<List<Card>> hands = new List<List<Card>>();
-            hands.Add(new List<Card>());
+            List<Hand> hands = new List<Hand>();
+            hands.Add(new Hand());
             return hands;
+        }
+
+        public static void ReplaceDecks()
+        {
+            Deck = DiscardedDeck;
+            DiscardedDeck.Clear();
+            Shuffle(Deck);
         }
     }
 
@@ -83,9 +90,20 @@ namespace _31_Blackjack
         }
     }
 
+    class Hand
+    {
+        public List<Card> Cards { get; set; }
+        public bool Inactive { get; set; }
+
+        public Hand()
+        {
+            Cards = new List<Card>();
+        }
+    }
+
     class Croupier
     {
-        public List<List<Card>> Hands { get; set; }
+        public List<Hand> Hands { get; set; }
 
         public Croupier()
         {
@@ -94,12 +112,12 @@ namespace _31_Blackjack
 
         public bool Insurance()
         {
-            if (Hands[0][0].Name == "Ace")
+            if (Hands[0].Cards[0].Name == "Ace")
             {
-                if (GameElements.CurrentBalance - 0.5 * GameElements.Bet > 0)
+                if (GameElements.CurrentBalance - GameElements.Bet > 0)
                 {
-                    GameElements.CurrentBalance -= (int)(0.5 * GameElements.Bet);
-                    GameElements.Insurance = (int)(0.5 * GameElements.Bet);
+                    GameElements.CurrentBalance -= GameElements.Bet;
+                    GameElements.Insurance = GameElements.Bet;
                     GameElements.DrawSeparators("-");
                     Console.WriteLine("Current insurance: {0}", GameElements.Insurance);
                     GameElements.DrawSeparators("-");
@@ -115,22 +133,23 @@ namespace _31_Blackjack
         public bool CroupiersMove()
         {
             int points = 0;
-            foreach (Card card in Hands[0])
+            foreach (Card card in Hands[0].Cards)
                 points += card.Value;
             if (points <= 16)
             {
                 Console.WriteLine("Croupier: Draw");
+                GameElements.DrawSeparators("-");
                 return true;
             }
             else if (points == 21)
             {
                 Console.WriteLine("Croupier has blackjack");
+                GameElements.DrawSeparators("-");
                 GameElements.CurrentBalance += GameElements.Insurance;
                 return false;
             }
             else
             {
-                GameElements.DrawSeparators("-");
                 Console.WriteLine("Croupier: Stand");
                 GameElements.DrawSeparators("-");
                 return false;
@@ -140,20 +159,14 @@ namespace _31_Blackjack
 
     class Player
     {
-        public List<List<Card>> Hands { get; set; }
+        public List<Hand> Hands { get; set; }
 
         public Player()
         {
             Hands = GameElements.ResetPlayerAndCroupier();
         }
 
-        public void Hit(int i)
-        {
-            Hands[i].Add(GameElements.Deck[GameElements.Deck.Count - 1]);
-            GameElements.MoveToTheDiscardDeck();
-        }
-
-        public bool DoubleTheBet(bool split)
+        public bool DoubleTheBet()
         {
             if (GameElements.CurrentBalance - GameElements.Bet > 0)
             {
@@ -161,9 +174,6 @@ namespace _31_Blackjack
                 GameElements.Bet *= 2;
                 GameElements.DrawSeparators("-");
                 Console.WriteLine("Current bet: {0}", GameElements.Bet);
-                if(split)
-                    Console.WriteLine("You've splitted your hand. Now you have {0} hands", Hands.Count + 1);
-                GameElements.DrawSeparators("-");
                 return true;
             }
             Console.WriteLine("You don't have this much money");
@@ -172,20 +182,34 @@ namespace _31_Blackjack
 
         public bool Split(int i)
         {
-            if (String.Compare(Hands[i][0].Name, Hands[i][1].Name) == 0)
+            try
             {
-                if (DoubleTheBet(true))
+                if (String.Compare(Hands[i].Cards[0].Name, Hands[i].Cards[1].Name) == 0)
                 {
-                    Hands.Add(new List<Card>());
-                    Hands[i + 1].Add(Hands[i][1]);
-                    Hands[i].RemoveAt(1);
-
-                    return true;
+                    if (Hands[i].Cards.Count == 2)
+                    {
+                        if (DoubleTheBet())
+                        {
+                            Hands.Add(new Hand());
+                            Hands[i + 1].Cards.Add(Hands[i].Cards[1]);
+                            Hands[i].Cards.RemoveAt(1);
+                            Console.WriteLine("You've splitted your hand. Now you have {0} hands", Hands.Count);
+                            GameElements.DrawSeparators("-");
+                            return true;
+                        }
+                        return false;
+                    }
+                    Console.WriteLine("It's not your first move");
+                    return false;
                 }
+                Console.WriteLine("Your first two cards don't have the same value");
                 return false;
             }
-            Console.WriteLine("Your first two cards don't have the same value");
-            return false;
+            catch
+            {
+                Console.WriteLine("You don't even have two cards in this hand");
+                return false;
+            }
         }
     }
 
@@ -194,7 +218,6 @@ namespace _31_Blackjack
         Croupier Croupier { get; set; }
         Player Player { get; set; }
         bool RoundIsOver { get; set; }
-        List<bool> Stand { get; set; }
         int RoundNo { get; set; }
 
         public Game()
@@ -209,8 +232,6 @@ namespace _31_Blackjack
         {
             Croupier = new Croupier();
             Player = new Player();
-            Stand = new List<bool>();
-            Stand.Add(false);
             RoundIsOver = false;
         }
 
@@ -218,6 +239,8 @@ namespace _31_Blackjack
         {
             while (GameElements.CurrentBalance > 0)
             {
+                if(RoundNo != 1)
+                    Console.WriteLine();
                 GameElements.DrawSeparators("+");
                 Console.WriteLine("Round {0}", RoundNo);
                 GameElements.DrawSeparators("+");
@@ -226,17 +249,18 @@ namespace _31_Blackjack
                 Reset();
                 RoundNo++;
             }
+            Console.WriteLine("/nYou're out of money. Maybe next time Fortune will be in your favour");
         }
 
         void Round()
         {
             PlaceABet();
             FirstDraw();
-            while (!RoundIsOver)
+            do
             {
                 for (int i = 0; i < Player.Hands.Count; i++)
                 {
-                    if (!Stand[i])
+                    if (!Player.Hands[i].Inactive)
                     {
                         if (Player.Hands.Count > 1)
                         {
@@ -249,28 +273,25 @@ namespace _31_Blackjack
                         {
                             case 1:
                                 GameElements.DrawSeparators("-");
-                                Player.Hit(i);
-                                ShowAllCards(Player, i);
-                                GameElements.DrawSeparators("-");
-                                CheckIfBusted(Player.Hands[i], i);
+                                GetACard(Player, true, i);
+                                CheckIfBusted(Player.Hands[i].Cards, i, true);
                                 break;
                             case 2:
                                 GameElements.DrawSeparators("-");
                                 Console.WriteLine("Stand");
                                 GameElements.DrawSeparators("-");
-                                Stand[i] = true;
+                                Player.Hands[i].Inactive = true;
                                 break;
                             case 3:
-                                if (!Player.DoubleTheBet(false))
+                                if (!Player.DoubleTheBet())
                                     goto GetInput;
                                 GetACard(Player, true, i);
-                                CheckIfBusted(Player.Hands[i], i);
-                                Stand[i] = true;
+                                CheckIfBusted(Player.Hands[i].Cards, i, true);
+                                Player.Hands[i].Inactive = true;
                                 break;
                             case 4:
                                 if (!Player.Split(i))
                                     goto GetInput;
-                                Stand.Add(false);
                                 break;
                             case 5:
                                 if (!Croupier.Insurance())
@@ -278,20 +299,20 @@ namespace _31_Blackjack
                                 break;
                         }
                     }
-
-                    if (Stand.All(a => a))
+                    if (CheckIfAllInactive())
                     {
                         if (Croupier.CroupiersMove())
                         {
                             GetACard(Croupier, true, 0);
-                            CheckIfBusted(Croupier.Hands[0], 0);
+                            CheckIfBusted(Croupier.Hands[0].Cards, 0, false);
                         }
                         else
                             Win(i);
-                        Console.WriteLine("Current balance: {0}\n", GameElements.CurrentBalance);
                     }
                 }
-            } 
+            } while (!RoundIsOver);
+            ShowAllCards(Croupier, 0);
+            DisplayCurrentBlance();
         }
 
         #region Game phases
@@ -318,21 +339,25 @@ namespace _31_Blackjack
 
         void FirstDraw()
         {
-            //GetACard(Player, false, 0);
-            //GetACard(Player, true, 0);
-            Player.Hands[0].Add(new Card("2", 2));
-            Player.Hands[0].Add(new Card("2", 2));
+            GetACard(Player, false, 0);
+            GetACard(Player, true, 0);
             GetACard(Croupier, true, 0);
             GetACard(Croupier, false, 0);
-            GameElements.DrawSeparators("-");
         }
 
         void GetACard<T>(T receiver, bool show, int i)
         {
-            if(receiver is Croupier)
-                ((Croupier)(object)receiver).Hands[i].Add(GameElements.Deck[GameElements.Deck.Count - 1]);
-            if(receiver is Player)
-                ((Player)(object)receiver).Hands[i].Add(GameElements.Deck[GameElements.Deck.Count - 1]);
+            try
+            {
+                if (receiver is Croupier)
+                    ((Croupier)(object)receiver).Hands[i].Cards.Add(GameElements.Deck[GameElements.Deck.Count - 1]);
+                if (receiver is Player)
+                    ((Player)(object)receiver).Hands[i].Cards.Add(GameElements.Deck[GameElements.Deck.Count - 1]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                GameElements.ReplaceDecks();
+            }
             GameElements.MoveToTheDiscardDeck();
             if (show)
                 ShowAllCards(receiver, i);
@@ -350,17 +375,18 @@ namespace _31_Blackjack
             {
                 temp = ((Player)(object)source);
                 if(temp.Hands.Count == 1)
-                    Console.WriteLine("Your hand: ");
+                    Console.WriteLine("Cards in hand: ");
                 else
-                    Console.WriteLine("Your hand no. {0}:", i);
+                    Console.WriteLine("Cards in  hand no. {0}:", i);
             }
-            for (int j = 0; j < temp.Hands[i].Count; j++)
+            for (int j = 0; j < temp.Hands[i].Cards.Count; j++)
             {
-                Console.Write(temp.Hands[i][j].Name);
-                if (j != temp.Hands[i].Count - 1)
+                Console.Write(temp.Hands[i].Cards[j].Name);
+                if (j != temp.Hands[i].Cards.Count - 1)
                     Console.Write(", ");
             }
             Console.WriteLine();
+            GameElements.DrawSeparators("-");
         }
 
         void PresentOptions()
@@ -381,7 +407,7 @@ namespace _31_Blackjack
             return temp;
         }
 
-        void CheckIfBusted(List<Card> Hand, int i)
+        void CheckIfBusted(List<Card> Hand, int i, bool player)
         {
             int points = 0;
             foreach (Card card in Hand)
@@ -398,28 +424,29 @@ namespace _31_Blackjack
                             return;
                     }
                 }
-                if (!Stand[i])
-                    PlayerLostRound(i);
+                if (player)
+                    PlayerBusted(i);
                 else
                 {
                     GameElements.DrawSeparators("!");
                     Console.WriteLine("Croupier busted");
                     GameElements.DrawSeparators("!");
+                    GameElements.CurrentBalance += 2 * GameElements.Bet;
                     RoundIsOver = true;
-                    GameElements.CurrentBalance += GameElements.Bet;
                 }
             }
         }
 
-        void PlayerLostRound(int i)
+        void PlayerBusted(int i)
         {
             if (Player.Hands.Count > 1)
             {
                 GameElements.DrawSeparators("!");
-                Console.WriteLine("Hand no {0} got busted", i);
+                Console.WriteLine("Hand no. {0} got busted", i);
                 GameElements.DrawSeparators("!");
-                Player.Hands.RemoveAt(i);
-                Stand[i] = true;
+                Player.Hands[i].Inactive = true;
+                if (CheckIfAllInactive())
+                    RoundIsOver = true;
             }
             else
             {
@@ -434,29 +461,45 @@ namespace _31_Blackjack
         {
             int playerPoints = 0;
             int croupierPoints = 0;
-            foreach (Card card in Croupier.Hands[0])
+            foreach (Card card in Croupier.Hands[0].Cards)
                 croupierPoints += card.Value;
-            foreach (Card card in Player.Hands[i])
+            foreach (Card card in Player.Hands[i].Cards)
                 playerPoints += card.Value;
+            GameElements.DrawSeparators("!");
             if (playerPoints > croupierPoints)
             {
-                GameElements.DrawSeparators("!");
                 if (Player.Hands.Count == 1)
                     Console.WriteLine("You win");
                 else
-                    Console.WriteLine("Hand no. wins");
-                GameElements.CurrentBalance += GameElements.Bet;
+                    Console.WriteLine("Hand no. {i} wins", i);
+                GameElements.CurrentBalance += 2 * GameElements.Bet / Player.Hands.Count;
             }
             else
             {
-                GameElements.DrawSeparators("!");
                 if (Player.Hands.Count == 1)
                     Console.WriteLine("You lose");
                 else
-                    Console.WriteLine("Hand no. loses");
+                    Console.WriteLine("Hand no. {0} loses", i);
             }
             GameElements.DrawSeparators("!");
             RoundIsOver = true;
+        }
+
+        bool CheckIfAllInactive()
+        {
+            foreach (Hand hand in Player.Hands)
+                if (!hand.Inactive)
+                    return false;
+            return true;
+        }
+
+        void DisplayCurrentBlance()
+        {
+            string temp = "Current balance: " + GameElements.CurrentBalance + "$#";
+            Console.WriteLine(temp);
+            for (int i = 0; i < temp.Length; i++)
+                Console.Write("#");
+            Console.WriteLine();
         }
         #endregion
     }
@@ -467,6 +510,8 @@ namespace _31_Blackjack
         {
             Game game = new Game();
 
+            Console.WriteLine("Welcome to the Casino! Check if Fortune smiles upon you today");
+            Console.WriteLine("You have {0}$\n", GameElements.CurrentBalance);
             game.Play();
 
             Console.ReadLine();
