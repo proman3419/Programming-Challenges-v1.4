35bfa978dfb98a32e84d2a7f069aa8a13d868941
Your Name <proman3419@gmail.com>
2018-08-01 17:19:46 +0200

    34 Placeholder

---
+++ 34 Genetic algorithms on polygons/34 Genetic algorithms on polygons/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34_Genetic_algorithms_on_polygons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
