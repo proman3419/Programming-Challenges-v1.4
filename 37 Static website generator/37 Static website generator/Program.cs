00f9ccbb07884a3210ee62b08393de0b66737a14
proman3419 <proman3419@gmail.com>
2018-08-28 22:22:37 +0200

    37 Placeholder

---
+++ 37 Static website generator/37 Static website generator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_Static_website_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
